267e08bc6752382e67a7a9a02117d74065de17bc
nadinestrella <nadinestrella@gmail.com>
2025-06-16 14:34:23 +0200

    added footer

--- country-app/src/app/app.ts
+++ country-app/src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent],
   templateUrl: './app.html',
-
 })
 export class App {
   protected title = 'country-app';
