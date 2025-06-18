137c286e6122a8c80e592dc19d9cd753d5d69a09
nadinestrella <nadinestrella@gmail.com>
2025-06-18 15:14:39 +0200

    added info to by country page

--- country-app/src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ country-app/src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,9 +1,9 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, resource, signal } from '@angular/core';
 import { SearchInputComponent } from '../../components/search-input/search-input.component';
 import { CountryListComponent } from '../../components/country-list/country-list.component';
 import { CountryService } from '../../services/country.service';
 
-import { Country } from '../../interfaces/country.interface';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -13,22 +13,35 @@ import { Country } from '../../interfaces/country.interface';
 export class ByCapitalPageComponent {
   //injectamos el country Service
   countryService = inject(CountryService);
+  query = signal('');
 
-  isLoading = signal(false);
-  isError = signal<string | null>(null);
-  countries = signal<Country[]>([]);
+  //es un recurso
+  countryResource = resource({
+    params: () => ({ query: this.query() }),
+    loader: async ({ params }) => {
+      if (!params.query) return [];
 
-  //creamos un metodo
-  onSearch(query: string) {
-    if (this.isLoading()) return;
+      return await firstValueFrom(
+        this.countryService.searchByCapital(params.query)
+      );
+    },
+  });
 
-    this.isLoading.set(true);
-    this.isError.set(null);
+  // isLoading = signal(false);
+  // isError = signal<string | null>(null);
+  // countries = signal<Country[]>([]);
 
-    //necesario para lanzar la peticion
-    this.countryService.searchByCapital(query).subscribe((countries) => {
-      this.isLoading.set(false);
-      this.countries.set(countries);
-    });
-  }
+  // //creamos un metodo
+  // onSearch(query: string) {
+  //   if (this.isLoading()) return;
+
+  //   this.isLoading.set(true);
+  //   this.isError.set(null);
+
+  //   //necesario para lanzar la peticion
+  //   this.countryService.searchByCapital(query).subscribe((countries) => {
+  //     this.isLoading.set(false);
+  //     this.countries.set(countries);
+  //   });
+  // }
 }
--- country-app/src/app/country/services/country.service.ts
+++ country-app/src/app/country/services/country.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { RESTCountry } from '../interfaces/rest-countries.interface';
-import { map, Observable } from 'rxjs';
+import { catchError, map, Observable, throwError } from 'rxjs';
 import type { Country } from '../interfaces/country.interface';
 import { CountryMapper } from '../mappers/country.mapper';
 
@@ -19,10 +19,32 @@ export class CountryService {
 
   searchByCapital(query: string): Observable<Country[]> {
     query = query.toLowerCase();
-    return this.http
-      .get<RESTCountry[]>(`${API_URL}/capital/${query}`)
-      .pipe(
-        map((resp) => CountryMapper.mapRestCountryArrayToCountryArray(resp))
-      );
+
+    return this.http.get<RESTCountry[]>(`${API_URL}/capital/${query}`).pipe(
+      map((resp) => CountryMapper.mapRestCountryArrayToCountryArray(resp)),
+      catchError((error) => {
+        console.log('Error fetching ', error);
+
+        return throwError(
+          () => new Error(`Could not get capital with that query ${query}`)
+        );
+      })
+    );
+  }
+
+  searchByCountry(query: string): Observable<Country[]> {
+    const url = `${API_URL}/name/${query}`;
+    query = query.toLowerCase();
+
+    return this.http.get<RESTCountry[]>(url).pipe(
+      map((resp) => CountryMapper.mapRestCountryArrayToCountryArray(resp)),
+      catchError((error) => {
+        console.log('Error fetching ', error);
+
+        return throwError(
+          () => new Error(`Could not get countries with that query ${query}`)
+        );
+      })
+    );
   }
 }
